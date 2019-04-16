b10b9dec00c23e3e30465f863478e983bbac1644
hendisantika <hendisantika@yahoo.co.id>
2019-04-17 06:39:35 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootbankaccount/SpringbootBankAccountApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootbankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBankAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBankAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootbankaccount/SpringbootBankAccountApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootbankaccount;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootBankAccountApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
