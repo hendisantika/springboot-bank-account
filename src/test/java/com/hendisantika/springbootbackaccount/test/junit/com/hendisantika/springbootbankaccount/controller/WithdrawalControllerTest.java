648e93d271a56adf9034471bb5be5e48546d7cb0
hendisantika <hendisantika@yahoo.co.id>
2019-04-17 17:56:08 +0700

    #30 - Add testMaxWithdrawalForTheDay Test Case in WithdrawalControllerTest

--- src/test/java/com/hendisantika/springbootbackaccount/test/junit/com/hendisantika/springbootbankaccount/controller/WithdrawalControllerTest.java
+++ src/test/java/com/hendisantika/springbootbackaccount/test/junit/com/hendisantika/springbootbankaccount/controller/WithdrawalControllerTest.java
@@ -2,13 +2,19 @@ package com.hendisantika.springbootbankaccount.controller;
 
 import com.google.gson.Gson;
 import com.hendisantika.springbootbankaccount.domain.Account;
+import com.hendisantika.springbootbankaccount.domain.AccountTransaction;
+import com.hendisantika.springbootbankaccount.domain.TransactionType;
 import com.hendisantika.springbootbankaccount.domain.UserTransaction;
+import com.hendisantika.springbootbankaccount.util.AccountUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.BDDMockito.given;
@@ -41,6 +47,31 @@ public class WithdrawalControllerTest extends BaseControllerTests {
                 .andExpect(status().isOk()).andExpect(content().json("{\"success\":false,\"messages\":{\"message\":\"You have insufficient funds\",\"title\":\"Error\"},\"errors\":{},\"data\":{},\"httpResponseCode\":406}"));
 
     }
+
+    @Test
+    public void testMaxWithdrawalForTheDay() throws Exception {
+
+        AccountTransaction transaction = new AccountTransaction(TransactionType.WITHDRAWAL.getId(), 40000, new Date());
+        AccountTransaction transaction2 = new AccountTransaction(TransactionType.WITHDRAWAL.getId(), 5000, new Date());
+
+        List<AccountTransaction> list = new ArrayList<>();
+        list.add(transaction);
+        list.add(transaction2);
+
+        UserTransaction userTransaction = new UserTransaction(8000);
+        Gson gson = new Gson();
+        String json = gson.toJson(userTransaction);
+
+        given(this.accountService.findById(1L)).willReturn(Optional.of(new Account(400000)));
+
+        given(this.transactionsService.findByDateBetweenAndType(AccountUtils.getStartOfDay(new Date()),
+                AccountUtils.getEndOfDay(new Date()), TransactionType.WITHDRAWAL.getId())).willReturn(list);
+
+        this.mvc.perform(post("/withdrawal/").contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk()).andExpect(content().json("{\"success\":false,\"messages\":{\"message\":\"Withdrawal per day should not be more than $50K\",\"title\":\"Error\"},\"errors\":{},\"data\":{},\"httpResponseCode\":406}"));
+
+    }
+
 
 
 }
