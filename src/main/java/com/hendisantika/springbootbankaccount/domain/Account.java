b01afcc0b34b17fc423090e40f957d1fe96c343c
hendisantika <hendisantika@yahoo.co.id>
2019-04-17 07:03:26 +0700

    #3 - Add Account Domain

---
+++ src/main/java/com/hendisantika/springbootbankaccount/domain/Account.java
@@ -0,0 +1,48 @@
+package com.hendisantika.springbootbankaccount.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-bank-account
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-17
+ * Time: 07:03
+ */
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private double amount;
+
+    protected Account() {
+    }
+
+    public Account(double amount) {
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+}
