bded0c9b311655bb4bdd17c40e9bee4eab830ae7
hendisantika <hendisantika@yahoo.co.id>
2019-04-17 07:05:43 +0700

    #7 - Add AccountService

---
+++ src/main/java/com/hendisantika/springbootbankaccount/service/AccountService.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootbankaccount.service;
+
+import com.hendisantika.springbootbankaccount.domain.Account;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-bank-account
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-17
+ * Time: 07:05
+ */
+@Repository
+public interface AccountService extends CrudRepository<Account, Long> {
+
+    Account findOne(Long id);
+
+}
