2e6e518b147755a5d1473b5fa1e29371d44abebf
hendisantika <hendisantika@yahoo.co.id>
2019-04-17 07:06:54 +0700

    #9 - Add AccountUtils

---
+++ src/main/java/com/hendisantika/springbootbankaccount/util/AccountUtils.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootbankaccount.util;
+
+import org.apache.commons.lang.time.DateUtils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-bank-account
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-17
+ * Time: 07:06
+ */
+public class AccountUtils {
+    public static Date getEndOfDay(Date date) {
+        return DateUtils.addMilliseconds(DateUtils.ceiling(date, Calendar.DATE), -1);
+    }
+
+    public static Date getStartOfDay(Date date) {
+        return DateUtils.truncate(date, Calendar.DATE);
+    }
+}
