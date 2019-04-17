e65137c4c722b40a0cc0a987838138d27a7e7f9f
hendisantika <hendisantika@yahoo.co.id>
2019-04-17 07:04:06 +0700

    #4 - Add Account Transaction

---
+++ src/main/java/com/hendisantika/springbootbankaccount/domain/AccountTransaction.java
@@ -0,0 +1,69 @@
+package com.hendisantika.springbootbankaccount.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-bank-account
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-17
+ * Time: 07:03
+ */
+@Entity
+public class AccountTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int type;
+    private double amount;
+    private Date date;
+
+    protected AccountTransaction() {
+    }
+
+    public AccountTransaction(int type, double amount, Date date) {
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
+
