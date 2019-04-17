dd44860dae4cebd03c486240537717877ca3b82e
hendisantika <hendisantika@yahoo.co.id>
2019-04-17 07:05:06 +0700

    #6 - Add UserTransaction

---
+++ src/main/java/com/hendisantika/springbootbankaccount/domain/UserTransaction.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootbankaccount.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-bank-account
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-17
+ * Time: 07:04
+ */
+public class UserTransaction {
+    private double amount;
+
+    public UserTransaction() {
+    }
+
+    public UserTransaction(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
